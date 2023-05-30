036eca759f8a84fa62b3be04f3f06899491ad2bc
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-05-30 12:52:01 +0200

    custom methods in CarRepository

--- src/main/java/com/vavatech/springrestexample/car/CarRepository.java
+++ src/main/java/com/vavatech/springrestexample/car/CarRepository.java
@@ -1,6 +1,22 @@
 package com.vavatech.springrestexample.car;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
+
+    // custom methods
+    List<CarEntity> findAllByBrandAndModel(String brand, String model);
+
+    @Query("""
+           SELECT e from CarEntity e
+        """)
+    List<CarEntity> findCarsByCustomQuery();
+
+    @Query(value = """
+           SELECT * from CarEntity
+        """, nativeQuery = true)
+    List<CarEntity> findCarsByCustomQueryNativeQuery();
 }
